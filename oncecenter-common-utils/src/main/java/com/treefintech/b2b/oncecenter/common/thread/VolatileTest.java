48d9dea31e26dbe224271ece934474ddf8ab6d20
章帆 <zhangfan@treefinance.com.cn>
2019-07-28 13:25:09 +0800

    AbstractQueuedSynchronizer

---
+++ oncecenter-common-utils/src/main/java/com/treefintech/b2b/oncecenter/common/thread/VolatileTest.java
@@ -0,0 +1,14 @@
+package com.treefintech.b2b.oncecenter.common.thread;
+
+/**
+ * @author zhangfan
+ * @description
+ * @date 2019/7/24 下午8:49
+ *
+ *
+ * volatile 用于多线程共用。
+ *
+ **/
+public class VolatileTest {
+
+}
